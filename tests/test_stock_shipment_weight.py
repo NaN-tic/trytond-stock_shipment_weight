00ddc3c7a349ceff09e194cf3f32b97a84081f38
resteve <resteve@zikzakmedia.com>
2015-12-01 11:26:56 +0100

    Tests

--- tests/test_stock_shipment_weight.py
+++ tests/test_stock_shipment_weight.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part stock_shipment_weight module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the stock_shipment_weight module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockShipmentWeightTestCase(unittest.TestCase):
+class StockShipmentWeightTestCase(ModuleTestCase):
     'Test Stock Shipment Weight module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_shipment_weight')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_shipment_weight')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_shipment_weight'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         StockShipmentWeightTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
