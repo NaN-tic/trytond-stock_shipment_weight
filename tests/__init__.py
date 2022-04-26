ddb44328739691bedce2cff57a8cee96fb3f51fb
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:11:22 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# This file is part stock_shipment_weight module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-
-from .test_stock_shipment_weight import suite
-
-__all__ = ['suite']
