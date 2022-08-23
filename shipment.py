acd980cca54c90b6fd3c0aa430592e2ebbacdb87
Àngel Àlvarez <angel@nan-tic.com>
2022-08-23 17:23:28 +0200

    add mixin and add weight to internal shipment

--- shipment.py
+++ shipment.py
@@ -8,9 +8,7 @@ from trytond.pool import Pool, PoolMeta
 
 __all__ = ['ShipmentOut', 'ShipmentOutReturn']
 
-
-class ShipmentOut(metaclass=PoolMeta):
-    __name__ = 'stock.shipment.out'
+class ShipmentWeightMixin:
     weight_uom = fields.Many2One('product.uom', 'Weight Uom',
         domain=[('category', '=', Id('product', 'uom_cat_weight'))],
         states={
@@ -28,6 +26,22 @@ class ShipmentOut(metaclass=PoolMeta):
     weight_func = fields.Function(fields.Float('Weight',
             digits=(16, Eval('weight_digits', 2)),
             depends=['weight_digits']), 'on_change_with_weight_func')
+
+
+    @fields.depends('weight', 'weight_lines')
+    def on_change_with_weight_func(self, name=None):
+        if self.weight:
+            return self.weight
+        return self.weight_lines
+
+    @fields.depends('weight_uom')
+    def on_change_with_weight_digits(self, name=None):
+        if self.weight_uom:
+            return self.weight_uom.digits
+        return 2
+
+class ShipmentOut(ShipmentWeightMixin, metaclass=PoolMeta):
+    __name__ = 'stock.shipment.out'
 
     @classmethod
     def get_weight_lines(cls, shipments, names):
@@ -63,18 +77,42 @@ class ShipmentOut(metaclass=PoolMeta):
                 Decimal(str(10.0 ** -digits))))
         return {'weight_lines': wlines}
 
-    @fields.depends('weight', 'weight_lines')
-    def on_change_with_weight_func(self, name=None):
-        if self.weight:
-            return self.weight
-        return self.weight_lines
+class ShipmentInternal(ShipmentWeightMixin, metaclass=PoolMeta):
+    __name__ = 'stock.shipment.internal'
 
-    @fields.depends('weight_uom')
-    def on_change_with_weight_digits(self, name=None):
-        if self.weight_uom:
-            return self.weight_uom.digits
-        return 2
+    @classmethod
+    def get_weight_lines(cls, shipments, names):
+        pool = Pool()
+        Config = pool.get('stock.configuration')
+        Uom = pool.get('product.uom')
+        Move = pool.get('stock.move')
 
+        origins = Move._get_origin()
+        keep_origin = True if 'stock.move' in origins else False
+
+        config = Config(1)
+        if config.weight_uom:
+            default_uom = config.weight_uom
+        else:
+            default_uom, = Uom.search([('symbol', '=', 'g')], limit=1)
+
+        wlines = dict((s.id, 0.0) for s in shipments)
+        for shipment in shipments:
+            to_uom = shipment.weight_uom or default_uom
+            digits = shipment.weight_digits
+            weight = Decimal(0.0)
+            moves = (shipment.outgoing_moves
+                if (keep_origin and shipment.outgoing_moves)
+                else shipment.incoming_moves)
+            for move in moves:
+                if move.quantity and move.product and move.product.weight:
+                    from_uom = move.product.weight_uom
+                    weight += Decimal(Uom.compute_qty(from_uom,
+                        move.product.weight * move.quantity, to_uom,
+                        round=False))
+            wlines[shipment.id] = float(weight.quantize(
+                Decimal(str(10.0 ** -digits))))
+        return {'weight_lines': wlines}
 
 class ShipmentOutReturn(metaclass=PoolMeta):
     __name__ = 'stock.shipment.out.return'
