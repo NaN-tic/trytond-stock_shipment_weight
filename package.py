9bc7d0fee2b6509ea128ceab7be20544f2db0f4a
Raimon Esteve <raimonesteve@gmail.com>
2024-07-22 07:21:53 +0200

    Add __register__

--- package.py
+++ package.py
@@ -5,8 +5,9 @@ from trytond.model import fields
 from trytond.pyson import Bool, Eval, Id
 
 
-class PackageType(metaclass=PoolMeta):
-    __name__ = 'stock.package.type'
+class ManualWeightMixin:
+    __slots__ = ()
+
     manual_weight = fields.Float(
         "Manual Weight", digits='manual_weight_uom',
         help="The manual weight of the package when empty.")
@@ -17,16 +18,24 @@ class PackageType(metaclass=PoolMeta):
             'required': Bool(Eval('manual_weight')),
             })
 
+    @classmethod
+    def __register__(cls, module_name):
+        table = cls.__table_handler__(module_name)
 
-class Package(metaclass=PoolMeta):
+        # Migration from 6.8: rename weight to manual_weight
+        if (table.column_exist('weight')
+                and not table.column_exist('manual_weight')):
+            table.column_rename('weight', 'manual_weight')
+            table.column_rename('weight_uom', 'manual_weight_uom')
+        super().__register__(module_name)
+
+
+class PackageType(ManualWeightMixin, metaclass=PoolMeta):
+    __name__ = 'stock.package.type'
+
+
+class Package(ManualWeightMixin, metaclass=PoolMeta):
     __name__ = 'stock.package'
-    manual_weight = fields.Float('Manual Weight')
-    manual_weight_uom = fields.Many2One(
-        'product.uom', "Manual Weight UOM",
-        domain=[('category', '=', Id('product', 'uom_cat_weight'))],
-        states={
-            'required': Bool(Eval('weight')),
-            })
 
     @fields.depends('type')
     def on_change_type(self):
