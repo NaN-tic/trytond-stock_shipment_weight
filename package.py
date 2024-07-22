539972c9c3a08c946a99575f99aec08d7ab07d1a
Raimon Esteve <raimonesteve@gmail.com>
2024-07-22 10:32:47 +0200

    Get total measurements from manual_weight and add scenario test

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 from trytond.model import fields
 from trytond.pyson import Bool, Eval, Id
 
@@ -43,3 +43,27 @@ class Package(ManualWeightMixin, metaclass=PoolMeta):
         # weight_uom is not default option in stock_package module.
         if self.type:
             self.manual_weight_uom = self.type.manual_weight_uom
+
+    def get_total_measurements(self, name, round=True):
+        pool = Pool()
+        Uom = pool.get('product.uom')
+        field = name[len('total_'):]
+
+        # calc core mesurements (additional_weight + packaging_weight)
+        measurement = super().get_total_measurements(name, round)
+
+        # sum manual_weight
+        if name == 'total_weight':
+            if self.manual_weight:
+                # in case has weight (from internal_weight moves), replace to manual_weight
+                if self.weight:
+                    measurement -= Uom.compute_qty(
+                        self.weight_uom, self.weight,
+                        self.weight_uom, round=False)
+                measurement += Uom.compute_qty(
+                    self.manual_weight_uom, self.manual_weight,
+                    self.weight_uom, round=False)
+                if round:
+                    measurement = getattr(self, field + '_uom').round(measurement)
+
+        return measurement
